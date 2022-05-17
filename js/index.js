e28d91d526fb96538055d5d48c0a8ba12af73922
axdborges <axdborges@outlook.com>
2022-05-17 18:14:42 -0300

    changes

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ const data = [
       nameItem: "Lightweight Jacket",
       description:
         "Adicione um pouco de energia ao seu guarda-roupa de inverno com esta jaqueta vibrante...",
-      value: 100,
+      value: 100.00,
       add: "AddCart",
       tag: ["Agasalhos"],
     },
@@ -15,7 +15,7 @@ const data = [
       nameItem: "Black Hat",
       description:
         "O gorro Next.js chegou! Esta beldade bordada tem um ajuste confortável que garante que...",
-      value: 100,
+      value: 100.00,
       add: "AddCart",
       tag: ["Acessórios"],
     },
@@ -25,7 +25,7 @@ const data = [
       nameItem: "Mask",
       description:
         "Esta máscara facial durável é feita de duas camadas de tecido tratado e possui presilhas...",
-      value: 40,
+      value: 40.00,
       add: "AddCart",
       tag: ["Acessórios"],
     },
@@ -35,7 +35,7 @@ const data = [
       nameItem: "T-Shirt",
       description:
         "Esta t-shirt é imprescindível no seu guarda-roupa, combinando o caimento intemporal de...",
-      value: 100,
+      value: 100.00,
       add: "AddCart",
       tag: ["Camisetas"],
     },
@@ -45,7 +45,7 @@ const data = [
       nameItem: "Short-Sleeve T-Shirt",
       description:
         "Agora você encontrou a camiseta básica do seu guarda-roupa. É feito de um mais grosso...",
-      value: 100,
+      value: 100.00,
       add: "AddCart",
       tag: ["Camisetas"],
     },
@@ -55,7 +55,7 @@ const data = [
       nameItem: "Champion Packable Jacket",
       description:
         "Proteja-se dos elementos com esta jaqueta embalável Champion. Esta jaqueta de poliést...",
-      value: 100,
+      value: 100.00,
       add: "AddCart",
       tag: ["Agasalhos"],
     },
@@ -71,7 +71,7 @@ function adicionarCards (array) {
         <span class="category"><small>${array[0].tag[0]}</small></span>
         <h3 class="nomeProd">${array[0].nameItem}</h3>
         <p class="descript">${array[0].description}</p>
-        <p class="price">${array[0].value}</p>
+        <p class="price">R$ ${array[0].value},00</p>
         <button class="addCart" id="${array[0].id}">Adicionar ao carrinho</button>
     </div>
 
@@ -82,7 +82,7 @@ function adicionarCards (array) {
         <span class="category"><small>${array[1].tag[0]}</small></span>
         <h3 class="nomeProd">${array[1].nameItem}</h3>
         <p class="descript">${array[1].description}</p>
-        <p class="price">${array[1].value}</p>
+        <p class="price">R$ ${array[1].value},00</p>
         <button class="addCart" id="${array[1].id}">Adicionar ao carrinho</button>
     </div>
 
@@ -93,7 +93,7 @@ function adicionarCards (array) {
         <span class="category"><small>${array[2].tag[0]}</small></span>
         <h3 class="nomeProd">${array[2].nameItem}</h3>
         <p class="descript">${array[2].description}</p>
-        <p class="price">${array[2].value}</p>
+        <p class="price">R$ ${array[2].value},00</p>
         <button class="addCart" id="${array[2].id}">Adicionar ao carrinho</button>
     </div>
 
@@ -104,7 +104,7 @@ function adicionarCards (array) {
         <span class="category"><small>${array[3].tag[0]}</small></span>
         <h3 class="nomeProd">${array[3].nameItem}</h3>
         <p class="descript">${array[3].description}</p>
-        <p class="price">${array[3].value}</p>
+        <p class="price">R$ ${array[3].value},00</p>
         <button class="addCart" id="${array[3].id}">Adicionar ao carrinho</button>
     </div>
 
@@ -115,7 +115,7 @@ function adicionarCards (array) {
         <span class="category"><small>${array[4].tag[0]}</small></span>
         <h3 class="nomeProd">${array[4].nameItem}</h3>
         <p class="descript">${array[4].description}</p>
-        <p class="price">${array[4].value}</p>
+        <p class="price">R$ ${array[4].value},00</p>
         <button class="addCart" id ="${array[4].id}">Adicionar ao carrinho</button>
     </div>
 
@@ -126,7 +126,7 @@ function adicionarCards (array) {
         <span class="category"><small>${array[5].tag[0]}</small></span>
         <h3 class="nomeProd">${array[5].nameItem}</h3>
         <p class="descript">${array[5].description}</p>
-        <p class="price">${array[5].value}</p>
+        <p class="price">R$ ${array[5].value},00</p>
         <button class="addCart" id="${array[5].id}" >Adicionar ao carrinho</button>
     </div>`
 )   
@@ -142,6 +142,10 @@ const listaVitrine = document.querySelector(".contDosProdutos")
 const cartCorpo = document.querySelector(".divCarrinho")
 const cartNovo = cartCorpo.insertAdjacentHTML("beforeend", `<ul class='listaCart'> </ul>`)
 const listaCart = document.querySelector(".listaCart")
+let qtdCart = 0
+let totalVendas = 0
+const divTotal = listaCart.insertAdjacentHTML("afterend",`<div class="totalCart"> </div>`)
+const pegarDivTotal = document.querySelector(".totalCart")
 
 listaVitrine.addEventListener("click", identificaItem) 
 
@@ -154,32 +158,29 @@ function identificaItem (event) {
 }
 
 function adicionarCarrinho (button){ 
+
     cartZerado.classList.add("dentroCartClick")
     for(let i = 0; i <= data.length; i++){
         if (button.id == i ) {
             listaCart.insertAdjacentHTML("beforeend", 
-                `<li class= "newPdt">
-                    <img src= "${data[i-1].img}" class="imgCart">
-                    <h3 class="nomeCart"> ${data[i-1].nameItem} </h3>
-                    <br>
-                    <p class="priceCart"> ${data[i-1].value} </p>
-                    <p class="removeCart"> Remover item </p>
-                </li>`)
+             `<li class= "newPdt">
+                <img src= "${data[i-1].img}" class="imgCart">
+                <h3 class="nomeCart"> ${data[i-1].nameItem} </h3>
+                <br>
+                <p class="priceCart"> R$ ${data[i-1].value},00 </p>
+                <p class="removeCart"> Remover item </p>
+             </li>`)
+            
+          qtdCart += 1
+          totalVendas += data[i-1].value
         } 
+    
     }
+
+    pegarDivTotal.insertAdjacentHTML("beforeend", 
+       ` <p class="qtdDivCart"> Quantidade: ${qtdCart} </p>
+        <p class="totalDivCart"> Total: R$ ${totalVendas},00 </p>`)
+    
 }
 
-
-
-// Colocando a quantidade (usar afterend)
-
-    // const qtdCart = document.querySelector(".listaCart").length
-    // const precoNoCart = document.querySelector(".priceCart")
-    // const totalVendas = 0
-
-    // for (let i = 0; i < qtdCart; i++) {
-
-    //     totalVendas += precoNoCart[i]
-
-    // }
 
